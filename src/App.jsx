2ce1bcc8ec800d4e37ca643c3e50aee2d949a763
Ahmed Mansour <ahmedmans.salama@gmail.com>
2025-07-08 13:40:57 +0200

    update in app and card

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Card from './components/card'
 import Img_card from './components/Img_card'
 
 
+
 function App() {
 
 
@@ -14,54 +15,36 @@ function App() {
       <div className='bg-gray-400 p-10 border min-h-screen  w-full gap-7 flex flex-wrap items-center  justify-center'>
         <div>
           <Img_card type='m5'></Img_card>
-          <Card type='m5'>
-            <h1 className='text-4xl'>BMW M5 Competition</h1>
-            <p className='text-xl'>
-              • 4.4L V8 Twin-Turbo. <br />
-              • 617 HP with M xDrive. <br />
-              • 0 –100 km/h in 3.3 seconds.
-            </p>
-            <div className=' flex justify-center items-center flex-col'>
-              <Btn type='test'> Test Drive</Btn>
-              <Btn type='more'>More</Btn>
-            </div>
-          </Card>
+          <Card
+            type='m5'
+            title='BMW M5 Competition'
+            content='• 4.4L V8 Twin-Turbo.• 617 HP with M xDrive.• 0 –100 km/h in 3.3 seconds.'
+            button1='Test Drive'
+            button2='More'
+          />
         </div>
-
-
-
         <div>
           <Img_card type='x6'></Img_card>
-          <Card type='x6'>
-            <h1 className='text-4xl'>BMW X6 2024</h1>
-            <p className='text-xl'>
-              • 3.0L I6 TwinPower Turbo. <br />
-              • 375 HP with xDrive AWD. <br />
-              • 0 –100 km/h in 5.3 seconds.
-            </p>
-            <div className=' flex justify-center items-center flex-col'>
-              <Btn type='test'> Test Drive</Btn>
-              <Btn type='more'>More</Btn>
-            </div>
-          </Card>
+          <Card
+            type='x6'
+            title='BMW X6 2024'
+            content=' • 3.0L I6 TwinPower Turbo. • 375 HP with xDrive AWD.• 0 –100 km/h in 5.3 seconds. '
+            button1='Test Drive'
+            button2='more'
+          />
         </div>
 
 
 
         <div>
           <Img_card type='m8'></Img_card>
-          <Card type='m8'>
-            <h1 className='text-4xl'>BMW M8 Convertible</h1>
-            <p className='text-xl'>
-            • 4.4L V8 Twin-Turbo. <br />
-            • 617 HP with M xDrive.   <br />
-            • 0 –100 km/h in 3.2 seconds. 
-            </p>
-            <div className=' flex justify-center items-center flex-col'>
-              <Btn type='test'> Test Drive</Btn>
-              <Btn type='more'>More</Btn>
-            </div>
-          </Card>
+          <Card
+            type='m8'
+            title='BMW M8 Convertible'
+            content='• 4.4L V8 Twin-Turbo. • 617 HP with M xDrive. • 0 –100 km/h in 3.2 seconds.'
+            button1='Test Drive'
+            button2='More'
+          />
         </div>
       </div>
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,32 @@
-function Card(props) {
+import { Children } from "react";
+import Btn from "./btn";
+
+function Card({ type, title, content, button1, button2 }) {
     let br = '';
     let bg = '';
     let tc = '';
-    switch (props.type) {
-        case 'x6':
-            bg = 'bg-gradient-to-r from-gray-500 via-gray-800 to-gray-900';
-            break;
+    let sp = '4845';
+    switch (type) {
         case 'm5':
+            bg = 'bg-gradient-to-r from-gray-600 via-gray-800 to-gray-900';
+            break;
+        case 'x6':
             bg = 'bg-gradient-to-r from-gray-900 via-gray-900 to-gray-900'
             break;
         case 'm8':
-            bg = 'bg-gradient-to-l from-gray-500 via-gray-800 to-gray-900'
+            bg = 'bg-gradient-to-l from-gray-600 via-gray-800 to-gray-900'
     }
-    let style = `${bg} ${br} shadow-lg hover:shadow-xl/30 font-bold h-110 text-gray-200 rounded-t-2xl w-100  p-9 flex flex-col justify-between text-center `;
+    let style = `${bg} ${br}   shadow-lg hover:shadow-xl/30 font-bold h-110 text-gray-200 rounded-t-2xl w-100   `;
     return (
         <div className={style}>
-            {props.children}
+            <div className="   flex flex-col justify-between items-center text-center  h-full p-10" >
+                <h1 className="text-4xl">{title}</h1>
+                <p className="text-xl " >{content}</p>
+                <div className="  flex flex-col justify-center items-center ">
+                    <Btn type='test'>{button1}</Btn>
+                    <Btn type='more'>{button2}</Btn>
+                </div>
+            </div>
         </div>
     )
 }
